c7ac75499e416251005474ab233b2c480c0ba485
Stussi <luis.pereira@unigran.br>
2023-01-17 15:18:48 -0300

    Criação do SpringBoot na aplicação

--- colecaoGithub/src/main/java/com/stussi/colecaogithub/ColecaoGithub.java
+++ colecaoGithub/src/main/java/com/stussi/colecaogithub/ColecaoGithub.java
@@ -1,12 +1,16 @@
 package com.stussi.colecaogithub;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 /**
- *
+ * Arquivo de teste do MavenFramework
  * @author Luis Alberto Schwind Pedroso Stussi da Silva Pereira
  */
+@SpringBootApplication
 public class ColecaoGithub {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        SpringApplication.run(ColecaoGithub.class, args);
     }
 }
