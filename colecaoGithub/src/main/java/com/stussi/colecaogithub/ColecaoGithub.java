e72a95f0b34a6802fa17f8aecb07cca380dfa510
Stussi <luis.pereira@unigran.br>
2023-01-17 09:53:07 -0300

    Teste de commit usando github com Netbeans IDE

---
+++ colecaoGithub/src/main/java/com/stussi/colecaogithub/ColecaoGithub.java
@@ -0,0 +1,12 @@
+package com.stussi.colecaogithub;
+
+/**
+ *
+ * @author Luis Alberto Schwind Pedroso Stussi da Silva Pereira
+ */
+public class ColecaoGithub {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
